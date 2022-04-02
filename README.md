69bf827d9caff540e4dba43d1713889241cce708
isMelvs <melvinbayogo.dev@gmail.com>
2022-04-03 01:52:03 +0800

    create task tracer

---
+++ README.md
@@ -0,0 +1,44 @@
+# tasker
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Install json-server
+```
+npm i json-server
+```
+Create new file "db.json" on the main folder then add json data
+Go to package.json then create script to run json server
+```
+"json-server": "json-server --watch db.json --port 5000"
+
+npm run json-server
+```
+
+### Implement the Router
+```
+npm i vue-router@next
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### Youtube Reference
+See [Vue JS Crash Course](https://www.youtube.com/watch?v=qZXt1Aom3Cs&t=3401s).
---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="flex items-center justify-center m-4">
+        <div class="container-box">
+            <MainHeader @toggle-add-task="toggleAddTask" title="Task Tracker" :showAddTask="showAddTask" />
+            <router-view :showAddTask="showAddTask"></router-view>
+            <FooterComponent />
+        </div>
+    </div>
+</template>
+
+<script>
+import MainHeader from './components/MainHeader.vue'
+import FooterComponent from './components/FooterComponent.vue'
+
+export default {
+    name: 'App',
+    components: {
+        MainHeader,
+        FooterComponent
+    },
+    data() {
+        return {
+            showAddTask: false
+        }
+    },
+    methods: {
+        toggleAddTask() {
+            this.showAddTask = !this.showAddTask
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
